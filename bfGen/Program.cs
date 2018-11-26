926cb3b2d591b289776960f73d6b7fd2ef818568
terence-john @work <t-j@terence-john.co.uk>
2018-11-26 11:57:10 +0000

    Add project files.

---
+++ bfGen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bfGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
